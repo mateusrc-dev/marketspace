8bc12a914cf9b9ab672230723906979f36e1458f
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-03-26 11:30:32 -0300

    create theme and component Loading

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
-import { Text, View } from "react-native";
+import { Text, View, StatusBar } from "react-native";
 import {
   useFonts,
   Karla_700Bold,
   Karla_400Regular,
 } from "@expo-google-fonts/karla";
+import { NativeBaseProvider } from "native-base";
+import { Loading } from "@components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -12,8 +14,26 @@ export default function App() {
   });
 
   return (
-    <View>
-      {fontsLoaded ? <Text>Hello World!</Text> : <Text>Carregando...</Text>}
-    </View>
+    <NativeBaseProvider>
+      <View
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          flex: 1,
+          backgroundColor: "blue",
+        }}
+      >
+        <StatusBar
+          barStyle="dark-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {!fontsLoaded ? (
+          <Text style={{ color: "white" }}>Hello World!</Text>
+        ) : (
+          <Loading />
+        )}
+      </View>
+    </NativeBaseProvider>
   );
 }
