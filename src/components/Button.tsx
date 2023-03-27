0e84793f81c16ae136300024ff9ac02f612cf323
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-03-26 21:34:50 -0300

    create component Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,28 @@
 import { Button as ButtonNativeBase, IButtonProps, Text } from "native-base";
 
 type Props = IButtonProps & {
-  title: string,
-}
+  title: string;
+  variant?: "light" | "blue";
+};
 
-export function Button({title, ...props}: Props) {
+export function Button({ title, variant = "blue", ...props }: Props) {
   return (
-      <ButtonNativeBase {...props} w="full" rounded={6}>
-        <Text>{title}</Text>
-      </ButtonNativeBase>
-    )
+    <ButtonNativeBase
+      {...props}
+      w="full"
+      rounded={6}
+      bgColor={variant === "blue" ? "blue.200" : "gray.500"}
+      _pressed={variant === "blue" ? { bg: "blue.100" } : { bg: "gray.400" }}
+    >
+      <Text
+        color={variant === "blue" ? "gray.700" : "gray.200"}
+        fontWeight="bold"
+        fontFamily="body"
+        fontSize="sm"
+        p={1}
+      >
+        {title}
+      </Text>
+    </ButtonNativeBase>
+  );
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { Center, Image, Text, View, VStack } from "native-base";
+import { Center, Image, ScrollView, Text, View, VStack } from "native-base";
 import Logo from "@assets/logo.svg";
 import Marketspace from "@assets/marketspace.png";
 import { Input } from "@components/Input";
@@ -6,39 +6,59 @@ import { Button } from "@components/Button";
 
 export function SignIn() {
   return (
-    <VStack flex={1} bgColor="gray.600">
-      <Center mt="16" mb="20">
-        <View mb="3">
-          <Logo />
-        </View>
-        <Image
-          source={Marketspace}
-          alt="logo marketspace"
-          resizeMode="contain"
-        />
-        <Text fontFamily="body" color="gray.300" fontSize="sm">
-          Seu espaço de compra e venda
-        </Text>
-      </Center>
-
-      <Text
-        textAlign="center"
-        fontFamily="body"
-        color="gray.200"
-        fontSize="sm"
-        mb="4"
+    <ScrollView>
+      <VStack
+        flex={1}
+        bgColor="gray.600"
+        height={556}
+        roundedBottomLeft={24}
+        roundedBottomRight={24}
       >
-        Acesse sua conta
-      </Text>
-      <VStack space="4" mx="12">
-        <Input
-          placeholder="E-mail"
-          keyboardType="email-address"
-          autoCapitalize="none"
-        />
-        <Input placeholder="Senha" secure={true} mb="8" />
-        <Button title="Entrar" />
+        <Center mt="16" mb="20">
+          <View mb="3">
+            <Logo />
+          </View>
+          <Image
+            source={Marketspace}
+            alt="logo marketspace"
+            resizeMode="contain"
+          />
+          <Text fontFamily="body" color="gray.300" fontSize="sm">
+            Seu espaço de compra e venda
+          </Text>
+        </Center>
+
+        <Text
+          textAlign="center"
+          fontFamily="body"
+          color="gray.200"
+          fontSize="sm"
+          mb="4"
+        >
+          Acesse sua conta
+        </Text>
+        <VStack space="4" mx="12">
+          <Input
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <Input placeholder="Senha" secure={true} mb="8" />
+          <Button title="Entrar" />
+        </VStack>
       </VStack>
-    </VStack>
+      <VStack mx="12" mb="12" mt="12">
+        <Text
+          textAlign="center"
+          fontFamily="body"
+          color="gray.200"
+          fontSize="sm"
+          mb="4"
+        >
+          Ainda não tem acesso?
+        </Text>
+        <Button title="Criar uma conta" variant="light" />
+      </VStack>
+    </ScrollView>
   );
 }
