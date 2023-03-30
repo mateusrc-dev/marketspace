c07e2a4a912163cfd45dc5f7c23cbeb888da2b78
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-03-30 00:43:21 -0300

    finished screen AdDetails

--- src/screens/AnnouncementDetails.tsx
+++ src/screens/AnnouncementDetails.tsx
@@ -1,4 +1,12 @@
-import { HStack, IImageProps, Image, Text, View, VStack } from "native-base";
+import {
+  HStack,
+  IImageProps,
+  Image,
+  ScrollView,
+  Text,
+  View,
+  VStack,
+} from "native-base";
 import {
   ArrowLeft,
   Bank,
@@ -43,8 +51,8 @@ const slides = [
 ];
 
 export function AnnouncementDetails({ ...rest }: AdDetailsProps) {
-  const [type, setType] = useState();
-  const [acceptExchange, setAcceptExchange] = useState<boolean>(true);
+  const [type, setType] = useState<string>("new");
+  const [acceptExchange, setAcceptExchange] = useState<boolean>(false);
   const navigation = useNavigation();
 
   function handleReturnNavigation() {
@@ -66,100 +74,186 @@ export function AnnouncementDetails({ ...rest }: AdDetailsProps) {
   }
 
   return (
-    <VStack flex={1}>
-      <TouchableOpacity onPress={handleReturnNavigation}>
-        <ArrowLeft />
+    <VStack flex={1} bgColor="gray.600">
+      <TouchableOpacity
+        style={{ marginTop: 36, marginLeft: 24, marginBottom: 12 }}
+        onPress={handleReturnNavigation}
+      >
+        <ArrowLeft size={24} color="#1A181B" />
       </TouchableOpacity>
-      <AppIntroSlider
-        renderItem={renderSlides}
-        data={slides}
-        nextLabel=""
-        doneLabel=""
-        dotStyle={{
-          backgroundColor: "#F7F7F8",
-          opacity: 0.5,
-          width: 121,
-          height: 3,
-          marginBottom: -70,
-        }}
-        activeDotStyle={{
-          backgroundColor: "#F7F7F8",
-          width: 121,
-          height: 3,
-          marginBottom: -70,
-        }}
-      />
-      <HStack>
-        <Image
-          {...rest}
-          source={{ uri: "https://github.com/mateusrc-dev.png" }}
-          alt="avatar do usuário"
-          position="absolute"
-          rounded="full"
-          borderWidth="1"
-          borderColor="gray.700"
-          resizeMode="contain"
-          w={6}
-          h={6}
-          top="1"
-          left="1"
+      <View width="full" height="280">
+        <AppIntroSlider
+          renderItem={renderSlides}
+          data={slides}
+          nextLabel=""
+          doneLabel=""
+          dotStyle={{
+            backgroundColor: "#F7F7F8",
+            opacity: 0.5,
+            width: 121,
+            height: 3,
+            marginBottom: -70,
+          }}
+          activeDotStyle={{
+            backgroundColor: "#F7F7F8",
+            width: 121,
+            height: 3,
+            marginBottom: -70,
+          }}
         />
-        <Text>Mateus Carvalho</Text>
-      </HStack>
-      <View rounded="full" bgColor={type === "new" ? "blue.100" : "gray.200"}>
-        <Text color="#FFFFFF" fontSize="10" fontWeight="bold">
-          {type === "new" ? "NOVO" : "USADO"}
-        </Text>
       </View>
-      <HStack>
-        <Text fontFamily="heading">Bicicleta</Text>
-        <Text>
-          <Text>R$</Text>120,00
+      <ScrollView>
+        <HStack alignItems="center" space={"2"} mt="5" mx="6">
+          <Image
+            {...rest}
+            source={{ uri: "https://github.com/mateusrc-dev.png" }}
+            alt="avatar do usuário"
+            rounded="full"
+            borderWidth="2"
+            borderColor="blue.200"
+            resizeMode="contain"
+            w={6}
+            h={6}
+          />
+          <Text color="gray.100" fontSize="sm" fontFamily="body">
+            Mateus Carvalho
+          </Text>
+        </HStack>
+        <View
+          mx="6"
+          mt="6"
+          mb="2"
+          px="2"
+          py="0.5"
+          w="43"
+          rounded="full"
+          bgColor={type === "new" ? "blue.100" : "gray.200"}
+        >
+          <Text color="#FFFFFF" fontSize="10" fontWeight="bold">
+            {type === "new" ? "NOVO" : "USADO"}
+          </Text>
+        </View>
+        <HStack
+          mx="6"
+          alignItems="center"
+          justifyContent="space-between"
+          mb="2"
+        >
+          <Text
+            fontFamily="heading"
+            fontSize="lg"
+            fontWeight="bold"
+            color="gray.100"
+          >
+            Bicicleta
+          </Text>
+          <Text
+            color="blue.200"
+            fontSize="lg"
+            fontWeight="bold"
+            fontFamily="body"
+          >
+            <Text
+              color="blue.200"
+              fontSize="sm"
+              fontWeight="bold"
+              fontFamily="body"
+            >
+              R$
+            </Text>
+            120,00
+          </Text>
+        </HStack>
+        <Text mx="6" mb="6" fontSize="sm" color="gray.200" fontFamily="body">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis,
+          atque quisquam nemo unde laborum ex eveniet minima praesentium?
+          Asperiores commodi, voluptatum quos laborum error quasi nam est ea
+          dignissimos recusandae.
         </Text>
-      </HStack>
-      <Text>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis,
-        atque quisquam nemo unde laborum ex eveniet minima praesentium?
-        Asperiores commodi, voluptatum quos laborum error quasi nam est ea
-        dignissimos recusandae. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Reiciendis nihil eaque nobis voluptatibus aspernatur.
-        Temporibus consectetur qui corporis minus ducimus, ipsam dignissimos,
-        quos sunt suscipit totam deserunt amet alias aliquam?
-      </Text>
-      <HStack>
-        <Text>Aceita troca?</Text>
-        <Text>{acceptExchange ? "Sim" : "Não"}</Text>
-      </HStack>
-      <Text>Meios de pagamento:</Text>
-      {/*aqui vamos fazer uma iteração com o array que possui os modos de pagamento*/}
-      <HStack>
-        <Barcode size={18} />
-        <Text>Boleto</Text>
-      </HStack>
-      <HStack>
-        <QrCode size={18} />
-        <Text>Pix</Text>
-      </HStack>
-      <HStack>
-        <Money size={18} />
-        <Text>Dinheiro</Text>
-      </HStack>
-      <HStack>
-        <CreditCard size={18} />
-        <Text>Cartão de Crédito</Text>
-      </HStack>
-      <HStack>
-        <Bank size={18} />
-        <Text>Depósito Bancário</Text>
-      </HStack>
-      <HStack>
-        <Text>
-          <Text>R$</Text>120,00
+        <HStack mx="6" alignItems="center" space="2" mb="4">
+          <Text
+            color="gray.200"
+            fontWeight="bold"
+            fontFamily="body"
+            fontSize="sm"
+          >
+            Aceita troca?
+          </Text>
+          <Text color="gray.200" fontFamily="body" fontSize="sm">
+            {acceptExchange ? "Sim" : "Não"}
+          </Text>
+        </HStack>
+        <Text
+          mx="6"
+          color="gray.200"
+          fontWeight="bold"
+          fontFamily="body"
+          fontSize="sm"
+          mb="2"
+        >
+          Meios de pagamento:
         </Text>
-        <ButtonComponent title="Entrar em contato" variant="blue">
-          <WhatsappLogo />
-        </ButtonComponent>
-      </HStack>
+        {/*aqui vamos fazer uma iteração com o array que possui os modos de pagamento*/}
+        <HStack mx="6" alignItems="center" space="2">
+          <Barcode size={18} color="#1A181B" />
+          <Text color="gray.200" fontSize="sm" fontFamily="body">
+            Boleto
+          </Text>
+        </HStack>
+        <HStack mx="6" alignItems="center" space="2">
+          <QrCode size={18} color="#1A181B" />
+          <Text color="gray.200" fontSize="sm" fontFamily="body">
+            Pix
+          </Text>
+        </HStack>
+        <HStack mx="6" alignItems="center" space="2">
+          <Money size={18} color="#1A181B" />
+          <Text color="gray.200" fontSize="sm" fontFamily="body">
+            Dinheiro
+          </Text>
+        </HStack>
+        <HStack mx="6" alignItems="center" space="2">
+          <CreditCard size={18} color="#1A181B" />
+          <Text color="gray.200" fontSize="sm" fontFamily="body">
+            Cartão de Crédito
+          </Text>
+        </HStack>
+        <HStack mx="6" alignItems="center" space="2">
+          <Bank size={18} color="#1A181B" />
+          <Text color="gray.200" fontSize="sm" fontFamily="body">
+            Depósito Bancário
+          </Text>
+        </HStack>
+        <HStack
+          px="6"
+          width="full"
+          mt="7"
+          h="90"
+          bgColor="gray.700"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Text
+            color="blue.200"
+            fontSize="xl"
+            fontWeight="bold"
+            fontFamily="body"
+          >
+            <Text
+              color="blue.200"
+              fontSize="sm"
+              fontWeight="bold"
+              fontFamily="body"
+            >
+              R$
+            </Text>
+            120,00
+          </Text>
+          <ButtonComponent title="Entrar em contato" variant="blue">
+            <WhatsappLogo weight="fill" color="#EDECEE" size="16" />
+          </ButtonComponent>
+        </HStack>
+      </ScrollView>
     </VStack>
   );
 }
