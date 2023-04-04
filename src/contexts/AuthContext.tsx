8f3097381c9c33e52b2f1e239220dbbcbf63ab59
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-04-04 12:16:25 -0300

    save user data in storage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import { UserDTO } from "@dtos/UserDTO";
 import { api } from "@services/api";
+import { storageUserSave } from "@storage/storageUser";
 import { ReactNode, createContext, useState } from "react";
 
 export type AuthContextDataProps = {
@@ -23,6 +24,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       const { data } = await api.post("/sessions", { email, password });
 
       if (data.user) {
+        storageUserSave(data.user);
         setUser(data.user);
       }
     } catch (error) {
